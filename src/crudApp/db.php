5b3785620cf74efc8c647bba5d790c2d531c9484
Deepak KC <deepak.curator@gmail.com>
2021-12-15 21:01:52 +0200

    Updates
    Adding folders and files

---
+++ src/crudApp/db.php
@@ -0,0 +1,11 @@
+<?php
+$servername="db";
+$username="root";
+$password="password";
+$dbname ="deepak";
+// creating connection 
+$conn = new mysqli("$servername", $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) 
+    die("Connection failed: ".$conn->connect_error);
+?>
